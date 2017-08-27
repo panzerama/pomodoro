9edf83d2600fb19aee75f3d374136bc98be014cf
JD Panzer <jason.drew.panzer@gmail.com>
2017-08-27 12:40:06 -0700

    gitignore file, requirements, basic object tests

--- core.py
+++ core.py
@@ -1,5 +1,10 @@
 class Task:
-    def __init__(self, name, duration):
+    def __init__(self, name, duration, notes=""):
         self.name = name
         self.duration = duration
+        self.notes = notes
 
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 import unittest
-from core import Task
+from core import Task, User
 
 
 class TaskFeatures(unittest.TestCase):
@@ -7,3 +7,11 @@ class TaskFeatures(unittest.TestCase):
         _task = Task("name", 30)
         self.assertEqual(_task.name, "name")
         self.assertEqual(_task.duration, 30)
+        self.assertEqual(_task.notes, "")
+
+class UserFeatures(unittest.TestCase):
+    def test_can_create_user(self):
+        _user = User("John Doe", "john.doe@example.com")
+        self.assertEqual(_user.name, "John Doe")
+        self.assertEqual(_user.email, "john.doe@example.com")
+
